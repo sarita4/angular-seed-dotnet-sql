961795743f531cc0541ebf6ba8c59be794ae5062
sarita4 <namdeosarita4@gmail.com>
2018-07-10 12:23:18 +0530

    Update prod.ts

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
   ENV: 'PROD',
-  API: 'https://devopsapp2.azurewebsites.net/api'
+  API: 'https://angularseeddotnet.azurewebsites.net/api'
 };
 
 export = ProdConfig;
